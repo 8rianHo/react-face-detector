d479a6fdcb15d0295b867f13229d60fc0e16adfd
8rianHo <brian8ho@googlemail.com>
2018-05-03 21:41:06 +0100

    image message update

--- src/components/imageURL/ImageURL.js
+++ src/components/imageURL/ImageURL.js
@@ -5,7 +5,7 @@ const ImageURL = ({ onInputChange, onButtonSubmit }) => {
   return(
     <div>
       <p className='f3'>
-        {'This application will detect faces in your pictures. Give it a url!'}
+        {'This application will detect faces in your pictures. Give it a photo url! (.jpg, .jpeg, .png)'}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
